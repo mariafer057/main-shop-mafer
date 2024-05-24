4ba7b7591bea99e77a6a3486490fb96048676951
mariafer057 <mariafernandaalves057@gmail.com>
2024-05-24 16:18:23 -0400

    commit

---
+++ src/app/page.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+export default async function Home() {
+const response = await fetch("https://fakestoreapi.com/products/");
+const data = await response.json();
+
+  return (
+    <main className={styles.main}>
+      {data.map((products)=> 
+            <div className={styles.card} key={products.id}>
+              <p>{products.title}</p>
+              <Image 
+              width= {300}
+              height={300}
+              src={products.image}/>
+            </div>
+      )}
+    </main>
+  );
+}
