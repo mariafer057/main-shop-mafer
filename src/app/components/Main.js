4f01f0ff26565c56bb7e681219b5893dd0171b60
mariafer057 <mariafernandaalves057@gmail.com>
2024-06-07 14:58:36 -0400

    07/06
    realizei o botão para buscar pela categoria de AZ e ZA, para que exibisse os produtos em ordem, realizei utilizando o sort

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,13 +1,65 @@
+'use client'
 import Image from "next/image";
 import Header from "./header";
 import NotFound from "../not-found";
 import styles from "./main.module.css"
-export default async function Main(){
-  const response = await fetch("https://fakestoreapi.com/products");
-  const data = await response.json();
-  return (
+import { useEffect, useState } from "react";
+export default function Main(){
+
+  const [listProdutos, setListaProdutos] = useState([]);
+  const [listPrecoMaior, setListaPrecoMaior] = useState([]);
+
+  useEffect(()=>{
+    const getProduct = async()=>{
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setListaProdutos(data);
+      setListaPrecoMaior(data);
+
+    }
+    getProduct();
+  }, []);
+
+
+    const orderAz = () =>{
+      const newList = [...listProdutos].sort((a, b)=>
+        a.title.localeCompare(b.title)
+      );
+      setListaProdutos(newList);
+    }
+
+
+    const orderZa = () =>{
+      const newList = [...listProdutos].sort((a, b)=>
+        b.title.localeCompare(a.title)
+      );
+      setListaProdutos(newList);
+    }
+
+   const precoMaior = () =>{
+      const newList = [...listPrecoMaior].sort((a, b)=>
+        b.price.localeCompare(a.price)
+      );
+      setListaPrecoMaior(newList);
+    }
+  
+  
+  return ( 
+    <>
     <main className={styles.main}>
-      {data.map((products)=> 
+    <div>
+    <button className={styles.botao} onClick={orderAz}>Az</button>
+    </div>
+
+    <div>
+    <button className={styles.botao} onClick={orderZa}>Za</button>
+    </div>
+
+    <div>
+    <button className={styles.botao} onClick={precoMaior}>+</button>
+    </div>
+
+      {listProdutos.map((products)=> 
             <div className={styles.card} key={products.id}>
               <h2 className={styles.titulo}>Título: {products.title}</h2>
               <h2>Preço: {products.price}</h2>
@@ -22,5 +74,6 @@ export default async function Main(){
             </div>
       )}
     </main>
+    </>
   );
 }
